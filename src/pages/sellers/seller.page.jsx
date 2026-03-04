aa4a29b233c36abb1a6949d66960217eee8bf6f2
Ashika <ashika.paudel_22@eemc.edu.np>
2026-03-04 08:35:06 +0545

    change in seller detail

--- src/pages/sellers/seller.page.jsx
+++ src/pages/sellers/seller.page.jsx
@@ -14,24 +14,25 @@ import SellerTable from "../../components/seller/sellerTable";
 
 const SellerPage = () => {
   return (
-    <>
+    <Box p={3}>
       <Typography
-        className="responsive_fontsize32"
-        variant="h4"
-        sx={{
-          p:1.5,
-          display: "flex",
-          justifyContent: "center",
-          color:"#1976d2",
-          fontWeight: "bold",
-          mt: { xs: 1.5, sm: 0 },  
-        }}
+        // className="responsive_fontsize32"
+        variant="h5"
+        fontWeight={600} mb={3}
+        // sx={{
+        //   p:1.5,
+        //   display: "flex",
+        //   justifyContent: "center",
+        //   color:"#1976d2",
+        //   fontWeight: "bold",
+        //   mt: { xs: 1.5, sm: 0 },  
+        // }}
       >
         Seller Details
       </Typography>
       <SellerTable/>
 
-    </>
+    </Box>
   );
 };
 
